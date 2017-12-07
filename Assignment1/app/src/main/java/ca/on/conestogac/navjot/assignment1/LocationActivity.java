4327ba9a8562e323185af5cdac4e554fb5fefa4b
Navjot-Nagi <31776059+Navjot-Nagi@users.noreply.github.com>
2017-12-07 15:33:37 -0500

    Showing Data for Location
    working on showing data for location activity

--- Assignment1/app/src/main/java/ca/on/conestogac/navjot/assignment1/LocationActivity.java
+++ Assignment1/app/src/main/java/ca/on/conestogac/navjot/assignment1/LocationActivity.java
@@ -6,18 +6,52 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.view.View;
 
+import java.util.List;
+
+import data.AppDatabase;
+import data.Location;
+
+
+
 public class LocationActivity extends AppCompatActivity {
+    private AppDatabase database;
+    private Location location;
+    private TextView nameOfLocation;
+    private TextView address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+
+        nameOfLocation = (TextView)
+                findViewById(R.id.locationNameLabel);
+        address = (TextView)
+                findViewById(R.id.userAddress);
+        database = AppDatabase.getDatabase(this.getApplication());
+
+    }
+
+    public void logInData()
+    {
+        String userNameString = nameOfLocation.getText().toString();
+        String passwordString = address.getText().toString();
+        List<Location> getLocation = database.locationDao().getLocation();
+
+        nameOfLocation.setText(getLocation.get(1).name);
+        address.setText("hello");
+
     }
 
     public void goBack(View view) {
         Intent main = new Intent(this, MainActivity.class);
         startActivity(main);
+    }
+
+    public void test(View view) {
+        logInData();
     }
 }
